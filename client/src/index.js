a61e40ccdfdd2d64b033cf51814dac98a49ee1c9
LeeYongMin01 <vbn0213@gmail.com>
2022-07-22 09:41:41 +0900

    render 오류 수정

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+// import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'antd/dist/antd.css';
+import 'antd/dist/antd.min.css';
 import {provider} from 'react-redux';
 import { applyMiddleware, createStore } from 'redux';
 import promiseMiddleware from 'redux-promise';
@@ -11,8 +12,9 @@ import ReduxThunk from 'redux-thunk';
 import Reducer from './_reducers';
 
 const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore);
+const root = createRoot(document.getElementById('root'));
 
-ReactDOM.render(
+root.render(
   <provider
    store = {createStoreWithMiddleware(Reducer,
     window.__REDUX_DEVTOOLS_EXTENSION__&&
@@ -20,8 +22,7 @@ ReactDOM.render(
     )}
   >
      <App/>
-  </provider>
-  , document.getElementById('root'));
+  </provider>);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
